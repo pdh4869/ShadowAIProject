bcc71865d084b8c7d20f366c9a531cf90b32787f
se-hyeonkim <michael_0903@naver.com>
2025-10-17 13:30:01 +0900

    Delete server directory

--- server/test_request.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:9000/mask-files/"
-files = {"Files": open(r"C:\Users\USER\Desktop\test.docx", "rb")}
-res = requests.post(url, files=files)
-print(res.status_code)
-print(res.json())
